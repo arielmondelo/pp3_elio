56958efcd4dbb842817269678724ecbc5c83ccec
Amiley Quintana <amileyquintana98@gmail.com>
2023-06-04 18:06:28 -0400

    Modificacion en los modelos

--- database/migrations/2023_06_04_013702_create_tipo_solicitudes_table.php
+++ database/migrations/2023_06_04_013702_create_tipo_solicitudes_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('tipo_solicitudes', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('codigo');
+            $table->string('nombre',100);
+            $table->string('codigo',10);
             $table->timestamps();
             $table->softDeletes();
         });
