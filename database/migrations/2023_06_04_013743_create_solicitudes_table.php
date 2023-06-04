e78a96670e61fb599af357dde3e9ad342f6273a2
Elio Galindo <eliogalindo92@yahoo.com>
2023-06-04 11:23:17 -0400

    Modificados metodos de login y logout

--- database/migrations/2023_06_04_013743_create_solicitudes_table.php
+++ database/migrations/2023_06_04_013743_create_solicitudes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('solicitudes', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('tipoSolicitud_id');
+            $table->unsignedBigInteger('entidad_id');
             $table->string('numeroExpediente', 50);
             $table->date('fechaInicio');
             $table->date('fechaFin');
@@ -25,6 +26,7 @@ return new class extends Migration
             $table->softDeletes();
 
             $table->foreign('tipoSolicitud_id')->references('id')->on('tipo_solicitudes')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('entidad_id')->references('id')->on('entidades')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
--- database/migrations/2023_06_04_013807_create_suplementos_table.php
+++ database/migrations/2023_06_04_013807_create_suplementos_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('suplementos', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('contrato_id');
-            $table->unsignedBigInteger('entidad_id');
             $table->string('numeroSuplemento');
             $table->date('fechaInicio');
             $table->date('fechaFin');
@@ -24,7 +23,6 @@ return new class extends Migration
             $table->softDeletes();
 
             $table->foreign('contrato_id')->references('id')->on('contratos')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('entidad_id')->references('id')->on('entidades')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
