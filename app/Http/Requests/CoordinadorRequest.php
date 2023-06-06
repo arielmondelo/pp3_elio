c563e2ecc3e5a6078be8a08bc515ee72f1818d9f
Amiley Quintana <amileyquintana98@gmail.com>
2023-06-05 23:18:11 -0400

    CoordinadorRequest

--- app/Http/Requests/CoordinadorRequest.php
+++ app/Http/Requests/CoordinadorRequest.php
@@ -11,7 +11,7 @@ class CoordinadorRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class CoordinadorRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'nombre' =>'string |required',
+            'apellidos'=>'string|required',
+            'telefono'=>'string |required',
+            'correo'=>'string|required|unique', 
         ];
     }
 }
